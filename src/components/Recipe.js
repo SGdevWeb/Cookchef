757cc44b67fc9583b3ed0c63980fa4865dc9b377
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-11-20 16:23:00 +0100

    added like icon

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
 
 function Recipe({ title, image }) {
+    const [liked, setLiked] = useState(false);
+
+    function handleClick() {
+        setLiked(!liked)
+    }
+
     return (
-        <div className={styles.recipe}>
+        <div onClick={handleClick} className={styles.recipe}>
             <div className={styles.imageContainer}>
                 <img src={image} alt="recipe" />
             </div>
-            <div className={`${styles.recipeTitle} d-flex flex-row justify-content align-items`}>
-                <h3>{title}</h3>
+            <div className={`${styles.recipeTitle} d-flex flex-column justify-content align-items`}>
+                <h3 className="mb-10">{title}</h3>
+                <i className={`fa-solid fa-heart ${liked ? styles.heartColor : ""}`}></i>
             </div>
         </div>
     )
