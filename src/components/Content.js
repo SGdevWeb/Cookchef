f53e93636504cb6988722b41247b8b02f6b96d84
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-11-21 18:55:17 +0100

    Added mobile menu and search bar

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,25 @@
 import styles from './Content.module.scss';
 import Recipe from './Recipe';
 import {data} from '../data/recipes';
+import { useState } from 'react';
 
 function Content() {
+    const [filter, setFilter] = useState('');
+
+    function handleInput(e) {
+        const filter = e.target.value;
+        setFilter(filter.trim().toLowerCase());
+    }
+
     return <div className="flex-fill container p-20">
         <h1 className='my-30'>Découvrez nos nouvelles recettes</h1>
-        <div className={`${styles.contentCard} card p-20`}>
+        <div className={`${styles.contentCard} card p-20`}>      
+            <div className={`d-flex flex-row justify-content align-items my-30 ${ styles.searchBar }`}>
+                <i className="fa-solid fa-magnifying-glass mr-15"></i>
+                <input onInput={ handleInput } className='flex-fill' type="text" placeholder='Rechercher' />
+            </div>
             <div className={styles.grid}>
-                {data.map(recipe =>
+                {data.filter( recipe => recipe.title.toLocaleLowerCase().startsWith(filter)).map(recipe =>
                     <Recipe
                         key={recipe._id} 
                         title={recipe.title} 
