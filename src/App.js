1b9efe02c4a789d5ca1804a848440564e14aa807
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-11-24 22:34:52 +0100

    Loading recipes from the API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Homepage from "./pages/Homepage/Homepage";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import styles from "./App.module.scss";
+// import {seedRecipes} from './data/seed';
+
+// seedRecipes();
 
 function App() {
   return (
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,35 +1,71 @@
 import styles from './Homepage.module.scss';
 import Recipe from './components/Recipe/Recipe';
-import {data} from '../../data/recipes';
-import { useState } from 'react';
+// import {data} from '../../data/recipes';
+import { useState, useEffect, useContext } from 'react';
+import Loading from '../../components/Loading/Loading';
+import { ApiContext } from '../../context/ApiContext';
 
-function Content() {
+function Homepage() {
+    const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [filter, setFilter] = useState('');
+    const BASE_URL_API = useContext(ApiContext);
+
+    useEffect(() => {
+        let cancel = false;
+        async function fetchRecipes() {
+            try {
+                setIsLoading(true);
+                const response = await fetch(BASE_URL_API);
+                if (response.ok && !cancel) {
+                    const recipes = await response.json();
+                    setRecipes(Array.isArray(recipes) ? recipes : [recipes])
+               }   
+            } catch (e) {
+                console.log('Erreur')
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchRecipes();
+        return () => (cancel = true);
+    }, [BASE_URL_API]);
 
     function handleInput(e) {
         const filter = e.target.value;
         setFilter(filter.trim().toLowerCase());
     }
 
-    return <div className="flex-fill container p-20">
-        <h1 className='my-30'>Découvrez nos nouvelles recettes</h1>
-        <div className={`${styles.contentCard} card p-20`}>      
-            <div className={`d-flex flex-row justify-content align-items my-30 ${ styles.searchBar }`}>
-                <i className="fa-solid fa-magnifying-glass mr-15"></i>
-                <input onInput={ handleInput } className='flex-fill' type="text" placeholder='Rechercher' />
-            </div>
-            <div className={styles.grid}>
-                {data.filter( recipe => recipe.title.toLocaleLowerCase().startsWith(filter)).map(recipe =>
-                    <Recipe
-                        key={recipe._id} 
-                        title={recipe.title} 
-                        image={recipe.image}
-                    />
+    return (
+        <div className="flex-fill container d-flex flex-column p-20">
+            <h1 className='my-30'>Découvrez nos nouvelles recettes</h1>
+            <div className={`${styles.contentCard} card flex-fill d-flex flex-column p-20 mb-20`}>      
+                <div className={`d-flex flex-row justify-content align-items my-30 ${ styles.searchBar }`}>
+                    <i className="fa-solid fa-magnifying-glass mr-15"></i>
+                    <input 
+                        onInput={ handleInput } 
+                        className='flex-fill' 
+                        type="text" 
+                        placeholder='Rechercher' />
+                </div>
+                {isLoading && !recipes.length ? (
+                    <Loading />
+                ) : (
+                    <div className={styles.grid}>
+                    {recipes
+                        .filter( recipe => recipe.title.toLocaleLowerCase().startsWith(filter))
+                        .map(recipe => (
+                            <Recipe
+                                key={recipe._id} 
+                                title={recipe.title} 
+                                image={recipe.image}
+                            />
+                        ))}
+                </div>
                 )}
             </div>
         </div>
-
-    </div>
+    );
 }
 
-export default Content;
+export default Homepage;
